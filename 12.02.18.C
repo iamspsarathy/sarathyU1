bb58228d3a602e626ae5f095104807ca00f8de6b
iamspsarathy <35688629+iamspsarathy@users.noreply.github.com>
2018-02-12 15:48:43 +0530

    Create 12.02.18.C

---
+++ 12.02.18.C
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+    int n,a,d,sum=0,i;
+    printf("enter the values");
+    scanf("%d%d%d",&n,&a,&d);
+    for(i=n;i>0;i--)
+    {
+    sum=sum+(a+(n-1)*d);
+    }
+    sum=sum+a;
+    printf("%d",sum);
+    return 0;
+}
